474a6fc8b03486b3dc549e277786708e526984f8
Waseem Dahman <dwaseem@icloud.com>
2019-01-10 00:56:07 -0500

    add types for the configured MusicKit instance

--- MusicKit-test.ts
+++ MusicKit-test.ts
@@ -17,3 +17,33 @@ music = MusicKit.configure({
 
 let instance: MusicKit.MusicKitInstance;
 instance = MusicKit.getInstance();
+
+music.bitrate.toString();
+music.developerToken.charCodeAt(0);
+music.storefrontId.charCodeAt(0);
+music.isAuthorized === true;
+music.musicUserToken.charCodeAt(0);
+music.playbackTargetAvailable === false;
+
+const api: MusicKit.API = music.api;
+const playbackState: MusicKit.PlaybackStates = music.playbackState;
+const player: MusicKit.Player = music.player;
+
+music.addEventListener('test', () => {});
+music.addToLibrary(1, 'type').then(() => {});
+music.authorize().then(token => token.charCodeAt(0));
+music.changeToMediaAtIndex(1).then(position => position > 1);
+music.play().then(position => position > 1);
+/**
+ * @todo SetQueueOptions
+ */
+music.playLater({}).then();
+music.playNext({});
+const queue: MusicKit.Queue = music.setQueue({});
+
+music.removeEventListener('test', () => {});
+music.seekToTime(12345).then(() => {});
+music.skipToNextItem().then(position => position > 1);
+music.skipToPreviousItem().then(position => position > 1);
+music.stop();
+music.unauthorize().then(() => {});
--- MusicKit.d.ts
+++ MusicKit.d.ts
@@ -2,9 +2,6 @@
 // Project: https://developer.apple.com/documentation/musickitjs
 // Definitions by: Waseem Dahman <https://github.com/wsmd/musickit-typescript>
 
-export = MusicKit;
-export as namespace MusicKit;
-
 /**
  * Use the MusicKit namespace to configure MusicKit JS and to access app
  * instances, control music playback, and fetch data from the Apple Music API.
@@ -51,7 +48,119 @@ declare namespace MusicKit {
    * This object provides access to a player instance, and through the player
    * instance, access to control playback.
    */
-  interface MusicKitInstance {}
+  interface MusicKitInstance {
+    /**
+     * An instance of the MusicKit API.
+     */
+    readonly api: API;
+    /**
+     * An instance of the MusicKit API.
+     */
+    readonly bitrate: MusicKit.PlaybackBitrate;
+    /**
+     * The developer token to identify yourself as a trusted developer and
+     * member of the Apple Developer Program.
+     */
+    readonly developerToken: string;
+    /**
+     * A Boolean value indicating whether the user has authenticated and
+     * authorized the application for use.
+     */
+    readonly isAuthorized: boolean;
+    /**
+     * A user token used to access personalized Apple Music content.
+     */
+    readonly musicUserToken: string;
+    /**
+     * The current playback state of the music player.
+     */
+    readonly playbackState: PlaybackStates;
+    /**
+     * A Boolean value that indicates if a playback target is available.
+     */
+    readonly playbackTargetAvailable: boolean;
+    /**
+     * An instance of the MusicKit player.
+     */
+    readonly player: Player;
+    /**
+     * The current storefront for the configured MusicKit instance.
+     */
+    readonly storefrontId: string;
+    /**
+     * Add an event listener for a MusicKit instance by name.
+     *
+     * @param name The name of the event.
+     * @param callback The callback function to invoke when the event occurs.
+     */
+    addEventListener(name: string, callback: () => any): void;
+    /**
+     * No description available.
+     */
+    addToLibrary(id: any, type: AddToLibraryContentType): Promise<any>;
+    /**
+     * Returns a promise containing a music user token when a user has
+     * authenticated and authorized the app.
+     */
+    authorize(): Promise<string>;
+    /**
+     * Begins playing the media item at the specified index in the queue.
+     *
+     * @param index The queue index to begin playing media.
+     */
+    changeToMediaAtIndex(index: number): Promise<number>;
+    /**
+     * Pauses playback of the media player.
+     */
+    pause(): void;
+    /**
+     * Begins playback of the current media item.
+     */
+    play(): Promise<MediaItemPosition>;
+    /**
+     * No description available.
+     */
+    playLater(options: SetQueueOptions): Promise<any>;
+    /**
+     * No description available.
+     */
+    playNext(options: SetQueueOptions): void;
+    /**
+     * Removes an event listener for a MusicKit instance by name.
+     *
+     * @param name The name of the event.
+     * @param callback The callback function to remove.
+     */
+    removeEventListener(name: string, callback: () => any): void;
+    /**
+     * Sets the playback point to a specified time.
+     *
+     * @param time The time to set as the playback point.
+     */
+    seekToTime(time: number): Promise<any>;
+    /**
+     * Sets a music player's playback queue using queue options.
+     *
+     * @param options The option used to set the playback queue.
+     */
+    setQueue(options: SetQueueOptions): Promise<Queue>;
+    /**
+     * Starts playback of the next media item in the playback queue.
+     */
+    skipToNextItem(): Promise<MediaItemPosition>;
+    /**
+     * Starts playback of the previous media item in the playback queue.
+     */
+    skipToPreviousItem(): Promise<MediaItemPosition>;
+    /**
+     * Stops playback of the media player.
+     */
+    stop(): void;
+    /**
+     * Unauthorizes the app for the current user.
+     */
+    unauthorize(): Promise<any>;
+  }
 
   /**
    * A configuration for an app.
@@ -76,10 +185,38 @@ declare namespace MusicKit {
   }
 
   /**
+   * @todo
+   */
+  interface Player {}
+
+  /**
+   * @todo
+   */
+  interface SetQueueOptions {}
+
+  /**
+   * @todo
+   */
+  interface Queue {}
+
+  /**
+   * @todo
+   */
+  interface PlaybackStates {}
+
+  /**
+   * @todo
+   */
+  interface API {}
+
+  /**
    * The playback bit rate of the music player.
    */
   enum PlaybackBitrate {
     HIGH = 256,
     STANDARD = 64,
   }
+
+  type AddToLibraryContentType = any;
+  type MediaItemPosition = number;
 }
