aa641cbcede8297337a3b56d50bd9b354e457282
Waseem Dahman <dwaseem@icloud.com>
2019-02-19 21:40:57 -0500

    rename types root

---
+++ MusicKit/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for MusicKit JS 1.2.3
+// Project: https://developer.apple.com/documentation/musickitjs
+// Definitions by: Waseem Dahman <https://github.com/wsmd>
+
+/// <reference path="MusicKit.d.ts" />
+/// <reference path="MusicKit.API.d.ts" />
+/// <reference path="MusicKit.Events.d.ts" />
+/// <reference path="MusicKit.Library.d.ts" />
+/// <reference path="MusicKit.MediaItem.d.ts" />
+/// <reference path="MusicKit.MKError.d.ts" />
+/// <reference path="MusicKit.MusicKitInstance.d.ts" />
+/// <reference path="MusicKit.Player.d.ts" />
+/// <reference path="MusicKit.Queue.d.ts" />
+/// <reference path="MusicKit.SetQueueOptions.d.ts" />
